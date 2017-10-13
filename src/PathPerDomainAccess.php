cc141f0346cc1ef0401a05cd3d1652a69086cd74
Jerick Duguran <jerick.duguran@movent.com>
2017-10-13 12:15:19 +0800

    - fixed route domain not to based on current request

--- src/PathPerDomainAccess.php
+++ src/PathPerDomainAccess.php
@@ -2,29 +2,31 @@
 
 namespace Drupal\pathperdomain;
 
-use Drupal\Core\Access\AccessResult;
-
-/**
+use Drupal\Core\Access\AccessResult; 
+use Drupal\Core\Routing\RouteMatch; 
+/**use
  * Custom access control handler for the domain path page.
  */
 class PathPerDomainAccess {
 
+	
   /**
    * Check if current path has alias
    *
    * @return \Drupal\Core\Access\AccessResult
    */
-  public function access() {
-    $domain = \Drupal::routeMatch()->getParameter('domain');
+  public function access(RouteMatch $routMatcher = null) {
+    $domain			= $routMatcher->getParameter('domain');
     $domain_current = \Drupal::service('domain.negotiator')->getActiveDomain();
-    $current_path = \Drupal::service('path.current')->getPath();
-    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $current_path	= \Drupal::service('path.current')->getPath();
+    $language		= \Drupal::languageManager()->getCurrentLanguage()->getId();
 
     $path_alias = \Drupal::service('path.alias_storage')->lookupPathAlias($current_path, $language);
+	
     if ($domain === $domain_current->id() && $path_alias) {
       return AccessResult::allowed();
-    }
-
+    } 
+	
     return AccessResult::forbidden();
   }
 }
