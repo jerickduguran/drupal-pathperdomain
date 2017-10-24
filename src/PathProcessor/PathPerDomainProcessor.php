73f1262fc55868e8969c79b54ddf1f3de2273bb2
Jerick Duguran <jerick.duguran@movent.com>
2017-10-24 17:30:58 +0800

    - fixed source route

--- src/PathProcessor/PathPerDomainProcessor.php
+++ src/PathProcessor/PathPerDomainProcessor.php
@@ -33,24 +33,52 @@ class PathPerDomainProcessor implements InboundPathProcessorInterface, OutboundP
   } 
   
   public function processOutbound($path, &$options = [], Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL){ 
-	     
-		$domainCurrent    = \Drupal::service('domain.negotiator')->getActiveDomain();
-		$pathAliasHelper  = \Drupal::service('pathauto.alias_storage_helper');
-		$languageManager  = \Drupal::languageManager();
+	    
+	$domainCurrent  = \Drupal::service('domain.negotiator')->getActiveDomain();  
+	$node_path		= \Drupal::service("path.alias_manager")->getPathByAlias($path);
+	$args			= explode('/', $node_path); 
+	$entity		    = \Drupal::entityTypeManager()->getStorage("node")->load(end($args));    
+			
+	if ($entity && $target_id = domain_source_get($entity)){
+		$source = \Drupal::service("domain.loader")->load($target_id);  
+		if($source->id() != $domainCurrent->id() && $domainCurrent->isDefault()){    
+			$options["base_url"] = trim($source->getRawPath(),"/");  
+			//try using node
+			$targetPath			= '/pathperdomain/'.$source->id().'/node/' . $entity->id();   
+			$domainPathEntities = $this->getDomainPathEntities($targetPath);
+			if(!empty($domainPathEntities)){   
+				return $domainPathEntities['alias'];
+			}   
+			//try using path 
+			$targetPath			= '/pathperdomain/' . $source->id().$path;   
+			$domainPathEntities = $this->getDomainPathEntities($targetPath);
+			if(!empty($domainPathEntities)){  
+				return $domainPathEntities['alias'];
+			}  
+		} 
+	}   	
+	 
+	$targetPath			= '/pathperdomain/' . $domainCurrent->id().$path;   
+	$domainPathEntities = $this->getDomainPathEntities($targetPath);
+		
+	if(!empty($domainPathEntities)){  
+		return $domainPathEntities['alias'];
+	}  
 
-		$targetPath			= '/pathperdomain/' . $domainCurrent->id() .$path; 
-		$domainPathEntities = $pathAliasHelper->loadBySource($targetPath,$languageManager->getCurrentLanguage()->getId());
-		
-		if(!empty($domainPathEntities)){  
-		    return $domainPathEntities['alias'];
-		}  
-		
-		// Cached URLs that have been processed by this outbound path 
-		if ($bubbleable_metadata) {
-		   $bubbleable_metadata 
-		   ->addCacheContexts(['url.query_args:pathperdomain']);
-		 } 
-	  
-	  return $path;
+	// Cached URLs that have been processed by this outbound path 
+	if ($bubbleable_metadata) {
+	   $bubbleable_metadata 
+	   ->addCacheContexts(['url.query_args:pathperdomain']);
+	} 
+	 
+	return $path;
+  }
+  
+  protected function getDomainPathEntities($path)
+  { 
+	  $pathAliasHelper  = \Drupal::service('pathauto.alias_storage_helper');
+	  $languageManager  = \Drupal::languageManager(); 
+	 
+	  return $pathAliasHelper->loadBySource($path,$languageManager->getCurrentLanguage()->getId()); 
   }
 }
